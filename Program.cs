e2d81801633cb0363ecc56f4a22d7977c5d2b1c9
Raul080922 <dennysmv37@uniandes.edu.ec>
2024-09-01 23:57:31 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Semana_5_Tarea_1.Views.Ciudades;
+namespace Semana_5_Tarea_1
+{
+    
+    static class Program
+    {
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frm_Lista_Ciudades());
+        }
+    }
+}
